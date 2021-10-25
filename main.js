8d3fe3d650f091958e18ad0bcd62c900b6b85a88
PaoloPascucci <pascuccip9@gmail.com>
2021-10-25 11:58:38 +0200

    Add ciclo

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const bici = [{
+const bici = [
+    {
 
     biciname: 'Bianchi',
     wheigth: 100,
 },
+
 {
     biciname: 'Dogma',
     wheigth: 70,
@@ -13,5 +15,17 @@ const bici = [{
     biciname: 'Ruggeri',
     wheigth: 120,
 }
-
 ]
+for (let i = 0; i < bici.length; i++) {
+    const BiciElement = bici[i]
+    console.log("Nome", BiciElement.biciname);
+    console.log("Peso", BiciElement.wheigth);
+
+}
+//stampare la bici con il peso minore
+//controllare quale bici pesa meno
+
+
+
+
+//document.getElementById('text').innerHTML = "la bici che pesa meno è " + biciname
