f8868618a9823f5c37230166412e32ab8d163239
PaoloPascucci <pascuccip9@gmail.com>
2021-10-26 12:51:03 +0200

    Add solution

--- snack 1 blocco 3/main.js
+++ snack 1 blocco 3/main.js
@@ -5,11 +5,50 @@
 * a somma degli elementi è minore di 50.
 */
 
-const Numbers = [];
+// const Numbers = [];
+// let SumNumbers = 0;
 
 
+// while (SumNumbers < 50) {
+//     const UserNumber = Number(prompt('insersci un numero'))
+//     SumNumbers += UserNumber;
+//     Numbers.push(UserNumber)
+// }
 
-const UserNumber = parseInt(prompt('insersci un numero'))
+// console.log(SumNumbers, Numbers);
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore utilizzando foreach
+
+const array = [
+    {
+        nome: 'Bianchi ',
+        peso: 10
+    },
+    {
+        nome: 'Bianchi2',
+        peso: 20
+    },
+    {
+        nome: 'Bianchi3 ',
+        peso: 5
+    },
+    {
+        nome: 'Bianchi4 ',
+        peso: 30
+    },
+    {
+        nome: 'Bianchi5 ',
+        peso: 15
+    }
+]
+let lightestBike = array[0]
 
 
-Numbers.push(UserNumber)
+array.forEach(bike => {
+    if (bike.peso < lightestBike.peso) {
+        lightestBike = bike
+    };
+});
+
+console.log(lightestBike);
